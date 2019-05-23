026d2ef74d91da66f45fd5563164378de39b1bb5
techsithgit <hemilp@tuplecorp.com>
2019-05-23 08:52:33 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import Outer from "./components/Outer";
+
+const nameInit = ["Archana", "Pushpa", "Deepthi", "peter", "john"];
 
 function App() {
+  const [names, setNames] = useState(nameInit);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {names.map(name => (
+          <Outer key={name} name={name} />
+        ))}
       </header>
     </div>
   );
